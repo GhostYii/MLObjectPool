c46810d74b33846a2ff3a19afd94408392691aa1
ghostyii <ghostyiigame@outlook.com>
2020-09-13 23:39:28 +0800

    refactor interface

--- MLObjectPool/Script/Core/Interfaces.cs
+++ MLObjectPool/Script/Core/Interfaces.cs
@@ -1,5 +1,35 @@
 ﻿namespace MLObjectPool
 {
+    public interface IAllocationHanlder
+    {
+        void OnAllocation(PoolBase pool);
+    }
+
+    public interface IRecycleHandler
+    {
+        void OnRecycle(PoolBase pool);
+    }
+
+    public interface IBeforeAllocationHandler
+    {
+        void OnBeforeAllocation(PoolBase pool);
+    }
+
+    public interface IBeforeRecycleHandler
+    {
+        void OnBeforeRecycle(PoolBase pool);
+    }
+
+    public interface IAfterAllocationHandler
+    {
+        void OnAfterAllocation(PoolBase pool);
+    }
+
+    public interface IAfterRecycleHandler
+    {
+        void OnAfterRecycle(PoolBase pool);
+    }
+
     public interface IPoolObjectBeforeHandler
     {
         void OnBeforeAllocation();
